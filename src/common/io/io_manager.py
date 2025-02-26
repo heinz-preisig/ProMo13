9cc32df3c1a99125ed2ab9ec5c6270346488490e
Alberto Rodriguez Fernandez <17274024+AlbertoRFer@users.noreply.github.com>
2025-02-26 14:40:57 +0100

    refactor(io): delegate OntologyContext management to new class
    - IOManager now delegates OntologyContext management to OntologyContextManger

--- src/common/io/io_manager.py
+++ src/common/io/io_manager.py
@@ -6,7 +6,7 @@ from typing import Protocol
 
 import attrs
 
-from .ontology_context import OntologyContext
+from .ontology_context import OntologyContext, OntologyContextManager
 
 
 class IOManager(Protocol):
@@ -16,80 +16,12 @@ class IOManager(Protocol):
 
 @attrs.define
 class DefaultIOManager:
-    _ontology_context: OntologyContext = attrs.field(
-        init=False, factory=OntologyContext
+    _ontology_context_manager: OntologyContextManager = attrs.field(
+        init=False, factory=OntologyContextManager
     )
 
     def get_ontology_context(self) -> OntologyContext:
-        context_copy = copy.deepcopy(self._ontology_context)
-        return context_copy
+        return self._ontology_context_manager.get_ontology_context()
 
     def update_ontology_context(self, context: OntologyContext) -> None:
-        repo_path = Path(context.repository_location)
-
-        if not repo_path.is_dir():
-            raise FileNotFoundError(f"The directory {repo_path} does not exist.")
-
-        mode = repo_path.stat().st_mode
-        is_writable = bool(mode & stat.S_IWUSR)
-        if not is_writable:
-            raise PermissionError(f"You have not write permissions on {repo_path}")
-
-        is_readable = bool(mode & stat.S_IRUSR)
-        if not is_readable:
-            raise PermissionError(f"You have not read permissions on {repo_path}")
-
-        self._ontology_context.repository_location = context.repository_location
-        ontologies_file_path = repo_path / ".ontologies.json"
-
-        if not ontologies_file_path.is_file():
-            NO_ONTOLOGIES = {"ontologies": []}
-            with ontologies_file_path.open("w") as file:
-                json.dump(NO_ONTOLOGIES, file)
-
-        with ontologies_file_path.open("r") as file:
-            data = json.load(file)
-
-        self._ontology_context.ontologies = data["ontologies"]
-
-        if not context.ontology_name == "":
-            if context.ontology_name not in self._ontology_context.ontologies:
-                raise FileNotFoundError(
-                    f"The ontology {context.ontology_name} does not exist in the repository at {repo_path}"
-                )
-            self._ontology_context.ontology_name = context.ontology_name
-
-            models_file_path = (
-                repo_path / context.ontology_name / "models" / ".models.json"
-            )
-            with models_file_path.open("r") as file:
-                data = json.load(file)
-
-            self._ontology_context.models = data["models"]
-
-        if not context.model_name == "":
-            if context.model_name not in self._ontology_context.models:
-                raise FileNotFoundError(
-                    f"The model {context.model_name} does not exist in the ontology {context.ontology_name}"
-                )
-            self._ontology_context.model_name = context.model_name
-
-            instantiations_file_path = (
-                repo_path
-                / context.ontology_name
-                / "models"
-                / context.model_name
-                / "instantiations"
-                / ".instantiations.json"
-            )
-            with instantiations_file_path.open("r") as file:
-                data = json.load(file)
-
-            self._ontology_context.instantiations = data["instantiations"]
-
-        if not context.instantiation_name == "":
-            if context.instantiation_name not in self._ontology_context.instantiations:
-                raise FileNotFoundError(
-                    f"The instantiation {context.instantiation_name} does not exist in the model {context.model_name}"
-                )
-            self._ontology_context.instantiation_name = context.instantiation_name
+        self._ontology_context_manager.update_ontology_context(context)
--- src/common/io/ontology_context.py
+++ src/common/io/ontology_context.py
@@ -1,4 +1,10 @@
+import copy
+import json
+import stat
 from dataclasses import dataclass, field
+from pathlib import Path
+
+import attrs
 
 
 @dataclass
@@ -10,3 +16,101 @@ class OntologyContext:
     ontologies: list[str] = field(init=False, default_factory=list)
     models: list[str] = field(init=False, default_factory=list)
     instantiations: list[str] = field(init=False, default_factory=list)
+
+
+@attrs.define
+class OntologyContextManager:
+    _ontology_context = attrs.field(init=False, factory=OntologyContext)
+
+    def get_ontology_context(self) -> OntologyContext:
+        context_copy = copy.deepcopy(self._ontology_context)
+        return context_copy
+
+    def update_ontology_context(self, context: OntologyContext) -> None:
+        update_dispatch = {
+            context.repository_location: self._update_repository_location,
+            context.ontology_name: self._update_ontology_name,
+            context.model_name: self._update_model_name,
+            context.instantiation_name: self._update_instantiation_name,
+        }
+
+        for value, update_method in update_dispatch.items():
+            if value:
+                update_method(value)
+
+    def _update_repository_location(self, repo_location: str) -> None:
+        repo_path = Path(repo_location)
+
+        if not repo_path.is_dir():
+            raise FileNotFoundError(f"The directory {repo_path} does not exist.")
+
+        mode = repo_path.stat().st_mode
+        is_writable = bool(mode & stat.S_IWUSR)
+        if not is_writable:
+            raise PermissionError(f"You have not write permissions on {repo_path}")
+
+        is_readable = bool(mode & stat.S_IRUSR)
+        if not is_readable:
+            raise PermissionError(f"You have not read permissions on {repo_path}")
+
+        self._ontology_context.repository_location = repo_location
+        ontologies_file_path = repo_path / ".ontologies.json"
+
+        if not ontologies_file_path.is_file():
+            NO_ONTOLOGIES = {"ontologies": []}
+            with ontologies_file_path.open("w") as file:
+                json.dump(NO_ONTOLOGIES, file)
+
+        with ontologies_file_path.open("r") as file:
+            data = json.load(file)
+
+        self._ontology_context.ontologies = data["ontologies"]
+
+    def _update_ontology_name(self, name: str) -> None:
+        repo_path = Path(self._ontology_context.repository_location)
+
+        if name not in self._ontology_context.ontologies:
+            raise FileNotFoundError(
+                f"The ontology {name} does not exist in the repository at {repo_path}"
+            )
+
+        self._ontology_context.ontology_name = name
+
+        models_file_path = repo_path / name / "models" / ".models.json"
+        with models_file_path.open("r") as file:
+            data = json.load(file)
+
+        self._ontology_context.models = data["models"]
+
+    def _update_model_name(self, name: str) -> None:
+        repo_path = Path(self._ontology_context.repository_location)
+        ontology_name = self._ontology_context.ontology_name
+
+        if name not in self._ontology_context.models:
+            raise FileNotFoundError(
+                f"The model {name} does not exist in the ontology {ontology_name}"
+            )
+
+        self._ontology_context.model_name = name
+
+        instantiations_file_path = (
+            repo_path
+            / ontology_name
+            / "models"
+            / name
+            / "instantiations"
+            / ".instantiations.json"
+        )
+        with instantiations_file_path.open("r") as file:
+            data = json.load(file)
+
+        self._ontology_context.instantiations = data["instantiations"]
+
+    def _update_instantiation_name(self, name: str) -> None:
+        model_name = self._ontology_context.model_name
+
+        if name not in self._ontology_context.instantiations:
+            raise FileNotFoundError(
+                f"The instantiation {name} does not exist in the model {model_name}"
+            )
+        self._ontology_context.instantiation_name = name
