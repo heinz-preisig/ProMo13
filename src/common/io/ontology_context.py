f9e6c0a373609ee7f7ca6121715b430b66c85c9f
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-02-28 15:37:02 +0100

    refactor(io): remove unnecessary attributes from OntologyContext

--- src/common/io/ontology_context.py
+++ src/common/io/ontology_context.py
@@ -8,9 +8,6 @@ class OntologyContext:
     ontology_name: str = ""
     model_name: str = ""
     instantiation_name: str = ""
-    ontologies: list[str] = field(init=False, default_factory=list)
-    models: list[str] = field(init=False, default_factory=list)
-    instantiations: list[str] = field(init=False, default_factory=list)
 
 
 class ContextMember(StrEnum):
