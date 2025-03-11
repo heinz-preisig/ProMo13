0ae1f62eaa8a74a83942d7d42b960baf7b5ece27
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-11 13:20:35 +0100

    refactor(io): change internal structure of the context manager
    change to show the dependency between the different attributes of IOContext
    refactor multiple methods of the context manager to adapt to the new structure

--- src/common/io/context_manager.py
+++ src/common/io/context_manager.py
@@ -1,22 +1,47 @@
-import copy
-
-import attrs
+import dataclasses
 
 from .context import IOContext, IOContextMember
 from .exceptions import IOContextError
 
+REPOSITORY_LOCATION_KEY = "repository_location"
+IO_CONTEXT_MEMBER_KEY = "{context_member}_name"
 
-@attrs.define
+
+@dataclasses.dataclass
+class IOContextNode:
+    parent: str
+    child: str
+    value: str = ""
+
+
 class IOContextManager:
-    _ontology_context = attrs.field(init=False, factory=IOContext)
+    def __init__(self) -> None:
+        key_ontology_name = self._get_context_member_key(IOContextMember.ONTOLOGY)
+        key_model_name = self._get_context_member_key(IOContextMember.MODEL)
+        key_instantiation_name = self._get_context_member_key(
+            IOContextMember.INSTANTIATION
+        )
 
-    def get_ontology_context(self) -> IOContext:
-        context_copy = copy.deepcopy(self._ontology_context)
-        return context_copy
+        self._nodes: dict[str, IOContextNode] = {
+            REPOSITORY_LOCATION_KEY: IOContextNode("", "ontology_name"),
+            key_ontology_name: IOContextNode("repository_location", "model_name"),
+            key_model_name: IOContextNode("ontology_name", "instantiation_name"),
+            key_instantiation_name: IOContextNode("model_name", ""),
+        }
 
-    def set_repository_location(self, repo_location: str) -> None:
-        self._ontology_context.repository_location = repo_location
-        self._reset_context_after_repo_location()
+    def _get_context_member_key(self, context_member: IOContextMember) -> str:
+        return IO_CONTEXT_MEMBER_KEY.format(context_member=context_member)
+
+    def get_repository_location(self) -> str:
+        return self._nodes[REPOSITORY_LOCATION_KEY].value
+
+    def get_context_member_name(self, context_member: IOContextMember) -> str:
+        key_name = self._get_context_member_key(context_member)
+        return self._nodes[key_name].value
+
+    def set_repository_location(self, repository_location: str) -> None:
+        self._nodes[REPOSITORY_LOCATION_KEY].value = repository_location
+        self._reset_dependent_context(REPOSITORY_LOCATION_KEY)
 
     def set_context_member_name(
         self,
@@ -25,15 +50,11 @@ class IOContextManager:
         options: list[str],
     ) -> None:
         self._verify_context_member_name(context_member, name, options)
-        match context_member:
-            case IOContextMember.ONTOLOGY:
-                self._ontology_context.ontology_name = name
-                self._reset_context_after_ontology_name()
-            case IOContextMember.MODEL:
-                self._ontology_context.model_name = name
-                self._reset_context_after_model_name()
-            case IOContextMember.INSTANTIATION:
-                self._ontology_context.instantiation_name = name
+
+        key_name = self._get_context_member_key(context_member)
+        self._nodes[key_name].value = name
+
+        self._reset_dependent_context(key_name)
 
     def _verify_context_member_name(
         self,
@@ -45,13 +66,20 @@ class IOContextManager:
             error_msg = f"Invalid {context_member} name: {name}"
             raise IOContextError(error_msg)
 
-    def _reset_context_after_repo_location(self) -> None:
-        self._ontology_context.ontology_name = ""
-        self._reset_context_after_ontology_name()
+    def _reset_dependent_context(self, key_name: str) -> None:
+        child_key = self._nodes[key_name].child
+        if not child_key:
+            return
 
-    def _reset_context_after_ontology_name(self) -> None:
-        self._ontology_context.model_name = ""
-        self._reset_context_after_model_name()
+        self._nodes[child_key].value = ""
+        self._reset_dependent_context(child_key)
 
-    def _reset_context_after_model_name(self) -> None:
-        self._ontology_context.instantiation_name = ""
+    def get_ontology_context(self) -> IOContext:
+        return IOContext(
+            repository_location=self.get_repository_location(),
+            ontology_name=self.get_context_member_name(IOContextMember.ONTOLOGY),
+            model_name=self.get_context_member_name(IOContextMember.MODEL),
+            instantiation_name=self.get_context_member_name(
+                IOContextMember.INSTANTIATION
+            ),
+        )
