55dd5e1bff24fcf90eebc6cabaa008729ddc738e
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-12 11:46:17 +0100

    refactor(io): merge permission checking methods

--- src/common/io/file_io.py
+++ src/common/io/file_io.py
@@ -31,8 +31,7 @@ class FileIO:
         self._check_dir_exists(repo_path)
 
         mode = repo_path.stat().st_mode
-        self._check_write_permissions(repo_path, mode)
-        self._check_read_permissions(repo_path, mode)
+        self._check_permissions(repo_path, mode)
 
     def _check_dir_exists(self, repo_path: Path) -> None:
         if not repo_path.is_dir():
@@ -41,23 +40,16 @@ class FileIO:
             exception_msg = f"Invalid repository location: {repo_path}"
             raise DataIOError(exception_msg)
 
-    def _check_write_permissions(self, repo_path: Path, mode: int) -> None:
-        WRITE_MODE = stat.S_IWUSR
-        is_writable = bool(mode & WRITE_MODE)
-        if not is_writable:
-            log_error_msg = f"You don't have write permissions: {repo_path}."
-            logger.error(log_error_msg)
-            exception_msg = f"Invalid repository location: {repo_path}"
-            raise DataIOError(exception_msg)
-
-    def _check_read_permissions(self, repo_path: Path, mode: int) -> None:
+    def _check_permissions(self, repo_path: Path, mode: int) -> None:
         READ_MODE = stat.S_IRUSR
-        is_readable = bool(mode & READ_MODE)
-        if not is_readable:
-            log_error_msg = f"You don't have read permissions: {repo_path}."
-            logger.error(log_error_msg)
-            exception_msg = f"Invalid repository location: {repo_path}"
-            raise DataIOError(exception_msg)
+        WRITE_MODE = stat.S_IWUSR
+        for mode_constant in [READ_MODE, WRITE_MODE]:
+            has_mode_permissions = bool(mode & mode_constant)
+            if not has_mode_permissions:
+                log_error_msg = f"You don't have the necessary permissions ({mode_constant}): {repo_path}."
+                logger.error(log_error_msg)
+                exception_msg = f"Invalid repository location: {repo_path}"
+                raise DataIOError(exception_msg)
 
     def get_context_member_options(
         self, context_member: IOContextMember, context: IOContext
