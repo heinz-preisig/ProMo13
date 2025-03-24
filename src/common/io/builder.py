460bbe029ca104e4664142c7f70e4de6efea7c9c
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-24 09:48:22 +0100

    refactor(io): split methods doing multiple things

--- src/common/io/builder.py
+++ src/common/io/builder.py
@@ -37,33 +37,44 @@ class IOBuilder:
 
     def build_index_map(self, data: typing.Any) -> None:
         new_index_map = {}
+        for idx_data in data:
+            new_index = self._build_new_index(idx_data)
+            new_index_map[new_index.identifier] = new_index
 
+        self.indices = new_index_map
+
+    def _build_new_index(self, idx_data: dict[str, typing.Any]) -> Index:
         try:
-            for idx_data in data:
-                new_index = self._strict_converter.structure(idx_data, Index)
-                new_index_map[new_index.identifier] = new_index
+            return self._strict_converter.structure(idx_data, Index)
         except cattrs.BaseValidationError as err:
             logger.error(err)
             raise exceptions.IOBuilderError("Corrupted Index file") from err
 
-        self.indices = new_index_map
-
     def build_variable_map(self, data: typing.Any) -> None:
-        new_variable_map = {}
-
         self._strict_converter.register_structure_hook(Index, validate_type)
 
-        try:
-            for var_data in data:
-                var_indices_ids = var_data["indices"]
-                var_indices = [self.indices[idx_id] for idx_id in var_indices_ids]
-                new_var_data = var_data | {"indices": var_indices}
-                new_variable = self._strict_converter.structure(new_var_data, Variable)
-                new_variable_map[new_variable.identifier] = new_variable
-        except cattrs.BaseValidationError as err:
-            logger.error(err)
-            raise exceptions.IOBuilderError("Corrupted Variable file") from err
+        new_variable_map = {}
+        for var_data in data:
+            new_var_data = self._replace_identifier_with_instances(var_data)
+            new_variable = self._build_new_variable(new_var_data)
+            new_variable_map[new_variable.identifier] = new_variable
+
+        self.variables = new_variable_map
 
         self._strict_converter.register_structure_hook(Index, cattrs.structure)
 
-        self.variables = new_variable_map
+    def _replace_identifier_with_instances(
+        self, var_data: dict[str, typing.Any]
+    ) -> dict[str, typing.Any]:
+        INDICES_KEY = "indices"
+        var_indices_ids = var_data[INDICES_KEY]
+        var_indices = [self.indices[idx_id] for idx_id in var_indices_ids]
+
+        return var_data | {INDICES_KEY: var_indices}
+
+    def _build_new_variable(self, var_data: dict[str, typing.Any]) -> Variable:
+        try:
+            return self._strict_converter.structure(var_data, Variable)
+        except cattrs.BaseValidationError as err:
+            logger.error(err)
+            raise exceptions.IOBuilderError("Corrupted Variable file") from err
