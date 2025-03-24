89144ae968a7d8c7d01a55a601fc71d15cfc9e17
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-24 09:48:22 +0100

    feat(tests(io)): enhance FakeIO to support multiple data sets

--- tests/unit/common/io/conftest.py
+++ tests/unit/common/io/conftest.py
@@ -4,8 +4,14 @@ import pytest
 
 from src.common.io import IOContext, IOContextMember, IOManager
 
+DEFAULT_ONTOLOGY_NAME = "default"
+
 
 class FakeDataIO:
+    def __init__(self) -> None:
+        self._index_data: dict[str, typing.Any] = {DEFAULT_ONTOLOGY_NAME: []}
+        self._variable_data: dict[str, typing.Any] = {DEFAULT_ONTOLOGY_NAME: []}
+
     def validate_repository_location(self, location: str) -> None:
         pass
 
@@ -20,17 +26,29 @@ class FakeDataIO:
             case IOContextMember.INSTANTIATION:
                 return ["VALID_INSTANTIATION", "instantiationOK"]
 
-    def set_index_data(self, data: typing.Any) -> None:
-        self._index_data = data
+    def set_index_data(
+        self, data: typing.Any, ontology_name: str = DEFAULT_ONTOLOGY_NAME
+    ) -> None:
+        self._index_data[DEFAULT_ONTOLOGY_NAME] = data
 
     def get_index_data(self, context: IOContext) -> typing.Any:
-        return self._index_data
+        is_ontology_name_set = bool(context.ontology_name)
+        if is_ontology_name_set:
+            return self._index_data[context.ontology_name]
 
-    def set_variable_data(self, data: typing.Any) -> None:
-        self._variable_data = data
+        return self._index_data[DEFAULT_ONTOLOGY_NAME]
+
+    def set_variable_data(
+        self, data: typing.Any, ontology_name: str = DEFAULT_ONTOLOGY_NAME
+    ) -> None:
+        self._variable_data[DEFAULT_ONTOLOGY_NAME] = data
 
     def get_variable_data(self, context: IOContext) -> typing.Any:
-        return self._variable_data
+        is_ontology_name_set = bool(context.ontology_name)
+        if is_ontology_name_set:
+            return self._variable_data[context.ontology_name]
+
+        return self._variable_data[DEFAULT_ONTOLOGY_NAME]
 
 
 @pytest.fixture
