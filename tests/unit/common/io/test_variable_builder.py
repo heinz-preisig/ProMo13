0d0973ea350a94581e08a9efcbd1a9735b414cfc
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-24 09:48:22 +0100

    refactor(tests(io)): extract map creation to helper functions

--- tests/unit/common/io/test_variable_builder.py
+++ tests/unit/common/io/test_variable_builder.py
@@ -7,10 +7,15 @@ from pytest_cases import case, fixture, parametrize, parametrize_with_cases
 from src.common.corelib import Index, IndexMap, Variable, VariableMap
 from src.common.io import IOBuilderError, IOContext, IOContextMember, IOManager
 
-INDEX_DATA = [
+INDEX_DATA_1 = [
     {"identifier": "I_1", "label": "node"},
     {"identifier": "I_2"},
     {"identifier": "I_3", "label": "interface", "iri": "promo:interface"},
+]
+INDEX_DATA_2 = [
+    {"identifier": "I_4"},
+    {"identifier": "I_5"},
+    {"identifier": "I_6"},
 ]
 
 GOOD_DATA: list[dict[str, typing.Any]] = [
@@ -31,31 +36,48 @@ BAD_DATA_WRONG_TYPE = [
     {"identifier": 3, "label": "n", "doc": "moles", "indices": ["I_1", "I_2"]},
 ]
 
+IDENTIFIER_KEY = "identifier"
+INDICES_KEY = "indices"
+
+
+def build_index_map(map_data: typing.Any) -> IndexMap:
+    indices = {}
+    for idx_data in map_data:
+        new_index = Index(**idx_data)
+
+        identifier = idx_data[IDENTIFIER_KEY]
+        indices[identifier] = new_index
+
+    return indices
+
+
+def build_variable_map(map_data: typing.Any, indices: IndexMap) -> VariableMap:
+    variables = {}
+    for var_data in map_data:
+        instanced_indices = [indices[idx] for idx in var_data[INDICES_KEY]]
+        instanced_var_data = var_data | {INDICES_KEY: instanced_indices}
+        new_var = Variable(**instanced_var_data)
+
+        identifier = var_data[IDENTIFIER_KEY]
+        variables[identifier] = new_var
+
+    return variables
+
 
 class CasesVariableBuilding:
     def valid_data_ok(
         self, fake_io_manager: IOManager, fake_data_io: FakeDataIO
     ) -> tuple[IOManager, VariableMap]:
-        fake_data_io.set_index_data(INDEX_DATA)
-        fake_data_io.set_variable_data(GOOD_DATA)
+        index_map_data = INDEX_DATA_1
+        variable_map_data = GOOD_DATA
 
-        indices = {}
-        for idx_data in INDEX_DATA:
-            identifier = idx_data["identifier"]
-            new_index = Index(**idx_data)  # type: ignore
-            indices[identifier] = new_index
+        fake_data_io.set_index_data(index_map_data)
+        fake_data_io.set_variable_data(variable_map_data)
 
-        expected_variable_map = {}
-        init_indices = {}
-        for var_data in GOOD_DATA:
-            identifier = var_data["identifier"]
-            if "indices" in var_data:
-                init_indices["indices"] = [indices[idx] for idx in var_data["indices"]]
+        indices = build_index_map(index_map_data)
+        expected_variables = build_variable_map(variable_map_data, indices)
 
-            new_var = Variable(**(var_data | init_indices))
-            expected_variable_map[identifier] = new_var
-
-        return fake_io_manager, expected_variable_map
+        return fake_io_manager, expected_variables
 
     @parametrize(
         data=(BAD_DATA_MISSING_REQUIRED, BAD_DATA_WRONG_TYPE),
@@ -65,7 +87,7 @@ class CasesVariableBuilding:
     def invalid_data(
         self, fake_io_manager: IOManager, fake_data_io: FakeDataIO, data: typing.Any
     ) -> IOManager:
-        fake_data_io.set_index_data(INDEX_DATA)
+        fake_data_io.set_index_data(INDEX_DATA_1)
         fake_data_io.set_variable_data(data)
 
         return fake_io_manager
