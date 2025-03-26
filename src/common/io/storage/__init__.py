994ea15ba2abe908d85707a6e0222ea9cc826b79
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-26 13:29:20 +0100

    refactor(io): rename file_io to file_storage

--- src/common/io/storage/__init__.py
+++ src/common/io/storage/__init__.py
@@ -1,5 +1,5 @@
 from src.common.io.storage.exceptions import DataIOError
-from src.common.io.storage.file_io import FileIO
+from src.common.io.storage.file_storage import FileIO
 from src.common.io.storage.protocols import GenericStorage
 
 __all__ = ["GenericStorage", "FileIO", "DataIOError"]
