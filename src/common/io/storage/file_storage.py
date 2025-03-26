bab88e9371d74b1167c345ea44353618fe234253
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-26 15:01:19 +0100

    refactor(io): extract repository validation into a new module

--- src/common/io/storage/file_storage.py
+++ src/common/io/storage/file_storage.py
@@ -1,19 +1,13 @@
-import logging
-import pathlib
-import stat
 import typing
 
 import attrs
 
 from src.common.io import context
 from src.common.io.storage import (
-    exceptions,
     json_file_reader_manager,
-    path_resolver,
     protocols,
+    repository_validation,
 )
-
-logger = logging.Logger(__name__)
 
 
 @attrs.define
@@ -23,31 +17,8 @@ class FileStorage:
     )
 
     def validate_repository_location(self, location: str) -> None:
-        new_context = context.IOContext(location)
-        template = path_resolver.PathTemplateStrings.ONTOLOGY_REPOSITORY_DIR
-        repo_path = path_resolver.resolve(new_context, template)
-        self._check_dir_exists(repo_path)
-
-        mode = repo_path.stat().st_mode
-        self._check_permissions(repo_path, mode)
-
-    def _check_dir_exists(self, repo_path: pathlib.Path) -> None:
-        if not repo_path.is_dir():
-            log_error_msg = f"The directory does not exist: {repo_path}"
-            logger.error(log_error_msg)
-            exception_msg = f"Invalid repository location: {repo_path}"
-            raise exceptions.DataIOError(exception_msg)
-
-    def _check_permissions(self, repo_path: pathlib.Path, mode: int) -> None:
-        READ_MODE = stat.S_IRUSR
-        WRITE_MODE = stat.S_IWUSR
-        for mode_constant in [READ_MODE, WRITE_MODE]:
-            has_mode_permissions = bool(mode & mode_constant)
-            if not has_mode_permissions:
-                log_error_msg = f"You don't have the necessary permissions ({mode_constant}): {repo_path}."
-                logger.error(log_error_msg)
-                exception_msg = f"Invalid repository location: {repo_path}"
-                raise exceptions.DataIOError(exception_msg)
+        repo_validator = repository_validation.FolderRepositoryValidator(location)
+        repo_validator.validate_repository_location()
 
     def get_context_member_options(
         self, context_member: context.IOContextMember, io_context: context.IOContext
