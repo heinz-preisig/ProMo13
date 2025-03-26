df0147d3f165b29f3dc3436e0e0980b58f4e3dbc
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-26 13:10:21 +0100

    refactor(io): switch to f-strings for string concatenation

--- src/common/io/data_io/path_resolver.py
+++ src/common/io/data_io/path_resolver.py
@@ -7,17 +7,17 @@ from src.common.io import context, exceptions
 
 class PathTemplateStrings(StrEnum):
     ONTOLOGY_REPOSITORY_DIR = "$repository_location/"
-    ONTOLOGY_DIR = ONTOLOGY_REPOSITORY_DIR + "$ontology_name/"
-    MODEL_LIBRARY_DIR = ONTOLOGY_DIR + "models/"
-    MODEL_DIR = MODEL_LIBRARY_DIR + "$model_name/"
-    INSTANTIATION_LIBRARY_DIR = MODEL_DIR + "instantiations/"
+    ONTOLOGY_DIR = f"{ONTOLOGY_REPOSITORY_DIR}$ontology_name/"
+    MODEL_LIBRARY_DIR = f"{ONTOLOGY_DIR}models/"
+    MODEL_DIR = f"{MODEL_LIBRARY_DIR}$model_name/"
+    INSTANTIATION_LIBRARY_DIR = f"{MODEL_DIR}instantiations/"
 
-    ONTOLOGY_INDEX_FILE = ONTOLOGY_REPOSITORY_DIR + ".repository_index.json"
-    MODEL_INDEX_FILE = MODEL_LIBRARY_DIR + ".repository_index.json"
-    INSTANTIATION_INDEX_FILE = INSTANTIATION_LIBRARY_DIR + ".repository_index.json"
-    INDEX_FILE = ONTOLOGY_DIR + "variables_v8.json"
-    VARIABLE_FILE = ONTOLOGY_DIR + "variables_v8.json"
-    EQUATION_FILE = ONTOLOGY_DIR + "variables_v8.json"
+    ONTOLOGY_INDEX_FILE = f"{ONTOLOGY_REPOSITORY_DIR}.repository_index.json"
+    MODEL_INDEX_FILE = f"{MODEL_LIBRARY_DIR}.repository_index.json"
+    INSTANTIATION_INDEX_FILE = f"{INSTANTIATION_LIBRARY_DIR}.repository_index.json"
+    INDEX_FILE = f"{ONTOLOGY_DIR}variables_v8.json"
+    VARIABLE_FILE = f"{ONTOLOGY_DIR}variables_v8.json"
+    EQUATION_FILE = f"{ONTOLOGY_DIR}variables_v8.json"
 
 
 def resolve(
