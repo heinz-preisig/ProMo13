a86e98798c2cb20d4ee4109d6e11162abcb1aa8f
Alberto Rodriguez Fernandez <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-27 15:40:12 +0100

    test(storage): rework equation_reading tests
    - Switch from testing the storage through the io_manager to test it directly.

--- tests/developer/common/io/storage/test_equation_reading.py
+++ tests/developer/common/io/storage/test_equation_reading.py
@@ -1,87 +1,56 @@
-import shutil
-from pathlib import Path
+import pathlib
 
 import pytest
-from pytest_cases import parametrize, parametrize_with_cases
+import pytest_cases
 
-from src.common.corelib import (
-    Equation,
-    EquationMap,
-    Index,
-    IndexMap,
-    Variable,
-    VariableMap,
+from src.common import io
+from src.common.io import storage
+
+EQUATION_DATA = [
+    {"identifier": "E_1", "variables": ["V_1", "V_2", "V_3"]},
+    {"identifier": "E_6", "variables": ["V_1", "V_2", "V_5", "V_6"]},
+    {"identifier": "E_2", "variables": ["V_4", "V_5", "V_6"]},
+    {"identifier": "E_3", "variables": ["V_5", "V_1", "V_7"]},
+    {"identifier": "E_4", "variables": ["V_8", "V_9", "V_10"]},
+    {"identifier": "E_5", "variables": ["V_9", "V_4", "V_11", "V_12"]},
+]
+
+
+ACCESS_ERROR = "Can not access equation data"
+CORRUPTED_ERROR = "Corrupted equation data"
+
+
+@pytest_cases.case(id="")
+@pytest_cases.parametrize(
+    "name_id, error_msg",
+    [("1", ACCESS_ERROR), ("2", CORRUPTED_ERROR), ("3", CORRUPTED_ERROR)],
+    ids=("missing file", "invalid json", "invalid json schema"),
 )
-from src.common.io import IOContextMember, IOManager
-from src.common.io.storage.exceptions import DataIOError
+def case_equation_file_problem(
+    ok_context: io.IOContext, name_id: str, error_msg: str
+) -> tuple[io.IOContext, str]:
+    ONTOLOGY_NAME = f"ontology{name_id}"
+    ok_context.ontology_name = ONTOLOGY_NAME
 
-INDEX_MAP = {
-    "I_1": Index("I_1", "iri_I1", "label_I1"),
-    "I_2": Index("I_2", "iri_I2", "label_I2"),
-    "I_3": Index("I_3", "iri_I3", "label_I3"),
-}
-I_1 = INDEX_MAP["I_1"]
-I_2 = INDEX_MAP["I_2"]
-I_3 = INDEX_MAP["I_3"]
+    return ok_context, error_msg
 
 
-VARIABLE_MAP = {
-    "V_1": Variable("V_1", "iri_V1", "label_V1", "doc_V1", [I_1, I_2]),
-    "V_2": Variable("V_2", "iri_V2", "label_V2", "doc_V2", []),
-    "V_3": Variable("V_3", "iri_V3", "label_V3", "doc_V3", [I_1]),
-    "V_4": Variable("V_4", "iri_V4", "label_V4", "doc_V4", [I_3]),
-    "V_5": Variable("V_5", "iri_V5", "label_V5", "doc_V5", [I_1]),
-    "V_6": Variable("V_6", "iri_V6", "label_V6", "doc_V6", [I_1]),
-    "V_7": Variable("V_7", "iri_V7", "label_V7", "doc_V7", [I_1]),
-    "V_8": Variable("V_8", "iri_V8", "label_V8", "doc_V8", [I_1]),
-    "V_9": Variable("V_9", "iri_V9", "label_V9", "doc_V9", [I_1]),
-    "V_10": Variable("V_10", "iri_V10", "label_V10", "doc_V10", [I_1]),
-    "V_11": Variable("V_11", "iri_V11", "label_V11", "doc_V11", [I_1]),
-    "V_12": Variable("V_12", "iri_V12", "label_V12", "doc_V12", [I_1]),
-}
-
-V_1 = VARIABLE_MAP["V_1"]
-V_2 = VARIABLE_MAP["V_2"]
-V_3 = VARIABLE_MAP["V_3"]
-V_4 = VARIABLE_MAP["V_4"]
-V_5 = VARIABLE_MAP["V_5"]
-V_6 = VARIABLE_MAP["V_6"]
-V_7 = VARIABLE_MAP["V_7"]
-V_8 = VARIABLE_MAP["V_8"]
-V_9 = VARIABLE_MAP["V_9"]
-V_10 = VARIABLE_MAP["V_10"]
-V_11 = VARIABLE_MAP["V_11"]
-V_12 = VARIABLE_MAP["V_12"]
-
-EQUATION_MAP = {
-    "E_1": Equation("E_1", [V_1, V_2, V_3]),
-    "E_2": Equation("E_2", [V_4, V_5, V_6]),
-    "E_3": Equation("E_3", [V_5, V_1, V_7]),
-    "E_4": Equation("E_4", [V_8, V_9, V_10]),
-    "E_5": Equation("E_5", [V_9, V_4, V_11, V_12]),
-    "E_6": Equation("E_6", [V_1, V_2, V_5, V_6]),
-}
-
-
-class CasesEquationReading:
-    def valid_data(
-        self, io_manager: IOManager, base_path: Path
-    ) -> tuple[IOManager, EquationMap]:
-        REPOSITORY_LOCATION = str(base_path / "repositoryOK")
-        ONTOLOGY_NAME = "ontologyOK"
-        io_manager.set_repository_location(REPOSITORY_LOCATION)
-        io_manager.set_context_member_name(IOContextMember.ONTOLOGY, ONTOLOGY_NAME)
-
-        return io_manager, EQUATION_MAP
-
-
-class TestEquationReading:
-    @parametrize_with_cases(
-        "manager, expected_equation_map", cases=CasesEquationReading, prefix="valid"
-    )
+class TestVariableReading:
     def test_equation_reading_ok(
-        self, manager: IOManager, expected_equation_map: EquationMap
+        self, test_storage: storage.GenericStorage, ok_context: io.IOContext
     ) -> None:
-        output_equation_map = manager.get_current_equation_map()
+        expected_data = EQUATION_DATA
 
-        assert expected_equation_map == output_equation_map
+        output_data = test_storage.get_equation_data(ok_context)
+
+        assert expected_data == output_data
+
+    @pytest_cases.parametrize_with_cases("test_context, error_msg", cases=".")
+    def test_exception_on_missing_equation_file(
+        self,
+        test_storage: storage.GenericStorage,
+        test_context: io.IOContext,
+        error_msg: str,
+    ) -> None:
+        with pytest.raises(io.DataIOError, match=error_msg):
+            test_storage.get_equation_data(test_context)
