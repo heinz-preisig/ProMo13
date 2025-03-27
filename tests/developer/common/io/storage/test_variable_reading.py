430fd58918de9a25584f191e9958ca6e71746015
Alberto Rodriguez Fernandez <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-27 15:22:45 +0100

    test(coverage): rework variable_reading tests
    - Switch from testing the storage through the io_manager to test it directly.

--- tests/developer/common/io/storage/test_variable_reading.py
+++ tests/developer/common/io/storage/test_variable_reading.py
@@ -1,98 +1,134 @@
-import shutil
-from pathlib import Path
+import pathlib
 
 import pytest
-from pytest_cases import parametrize, parametrize_with_cases
+import pytest_cases
 
-from src.common.corelib import Index, IndexMap, Variable, VariableMap
-from src.common.io import IOContextMember, IOManager
-from src.common.io.storage.exceptions import DataIOError
+from src.common import io
+from src.common.io import storage
 
-INDEX_MAP = {
-    "I_1": Index("I_1", "iri_I1", "label_I1"),
-    "I_2": Index("I_2", "iri_I2", "label_I2"),
-    "I_3": Index("I_3", "iri_I3", "label_I3"),
-}
-I_1 = INDEX_MAP["I_1"]
-I_2 = INDEX_MAP["I_2"]
-I_3 = INDEX_MAP["I_3"]
+VARIABLE_DATA = [
+    {
+        "identifier": "V_1",
+        "iri": "iri_V1",
+        "label": "label_V1",
+        "doc": "doc_V1",
+        "indices": ["I_1", "I_2"],
+    },
+    {
+        "identifier": "V_2",
+        "iri": "iri_V2",
+        "label": "label_V2",
+        "doc": "doc_V2",
+        "indices": [],
+    },
+    {
+        "identifier": "V_3",
+        "iri": "iri_V3",
+        "label": "label_V3",
+        "doc": "doc_V3",
+        "indices": ["I_1"],
+    },
+    {
+        "identifier": "V_4",
+        "iri": "iri_V4",
+        "label": "label_V4",
+        "doc": "doc_V4",
+        "indices": ["I_3"],
+    },
+    {
+        "identifier": "V_5",
+        "iri": "iri_V5",
+        "label": "label_V5",
+        "doc": "doc_V5",
+        "indices": ["I_1"],
+    },
+    {
+        "identifier": "V_6",
+        "iri": "iri_V6",
+        "label": "label_V6",
+        "doc": "doc_V6",
+        "indices": ["I_1"],
+    },
+    {
+        "identifier": "V_7",
+        "iri": "iri_V7",
+        "label": "label_V7",
+        "doc": "doc_V7",
+        "indices": ["I_1"],
+    },
+    {
+        "identifier": "V_8",
+        "iri": "iri_V8",
+        "label": "label_V8",
+        "doc": "doc_V8",
+        "indices": ["I_1"],
+    },
+    {
+        "identifier": "V_9",
+        "iri": "iri_V9",
+        "label": "label_V9",
+        "doc": "doc_V9",
+        "indices": ["I_1"],
+    },
+    {
+        "identifier": "V_10",
+        "iri": "iri_V10",
+        "label": "label_V10",
+        "doc": "doc_V10",
+        "indices": ["I_1"],
+    },
+    {
+        "identifier": "V_11",
+        "iri": "iri_V11",
+        "label": "label_V11",
+        "doc": "doc_V11",
+        "indices": ["I_1"],
+    },
+    {
+        "identifier": "V_12",
+        "iri": "iri_V12",
+        "label": "label_V12",
+        "doc": "doc_V12",
+        "indices": ["I_1"],
+    },
+]
 
 
-VARIABLE_MAP = {
-    "V_1": Variable("V_1", "iri_V1", "label_V1", "doc_V1", [I_1, I_2]),
-    "V_2": Variable("V_2", "iri_V2", "label_V2", "doc_V2", []),
-    "V_3": Variable("V_3", "iri_V3", "label_V3", "doc_V3", [I_1]),
-    "V_4": Variable("V_4", "iri_V4", "label_V4", "doc_V4", [I_3]),
-    "V_5": Variable("V_5", "iri_V5", "label_V5", "doc_V5", [I_1]),
-    "V_6": Variable("V_6", "iri_V6", "label_V6", "doc_V6", [I_1]),
-    "V_7": Variable("V_7", "iri_V7", "label_V7", "doc_V7", [I_1]),
-    "V_8": Variable("V_8", "iri_V8", "label_V8", "doc_V8", [I_1]),
-    "V_9": Variable("V_9", "iri_V9", "label_V9", "doc_V9", [I_1]),
-    "V_10": Variable("V_10", "iri_V10", "label_V10", "doc_V10", [I_1]),
-    "V_11": Variable("V_11", "iri_V11", "label_V11", "doc_V11", [I_1]),
-    "V_12": Variable("V_12", "iri_V12", "label_V12", "doc_V12", [I_1]),
-}
+ACCESS_ERROR = "Can not access variable data"
+CORRUPTED_ERROR = "Corrupted variable data"
 
 
-class CasesVariableReading:
-    def valid_data(
-        self, io_manager: IOManager, base_path: Path
-    ) -> tuple[IOManager, VariableMap]:
-        REPOSITORY_LOCATION = str(base_path / "repositoryOK")
-        ONTOLOGY_NAME = "ontologyOK"
-        io_manager.set_repository_location(REPOSITORY_LOCATION)
-        io_manager.set_context_member_name(IOContextMember.ONTOLOGY, ONTOLOGY_NAME)
+@pytest_cases.case(id="")
+@pytest_cases.parametrize(
+    "name_id, error_msg",
+    [("1", ACCESS_ERROR), ("2", CORRUPTED_ERROR), ("3", CORRUPTED_ERROR)],
+    ids=("missing file", "invalid json", "invalid json schema"),
+)
+def case_variable_file_problem(
+    ok_context: io.IOContext, name_id: str, error_msg: str
+) -> tuple[io.IOContext, str]:
+    ONTOLOGY_NAME = f"ontology{name_id}"
+    ok_context.ontology_name = ONTOLOGY_NAME
 
-        return io_manager, VARIABLE_MAP
-
-    def invalid_missing_index_file(
-        self, io_manager: IOManager, base_path: Path
-    ) -> tuple[IOManager, str]:
-        REPOSITORY_LOCATION = str(base_path / "repositoryOK")
-        ONTOLOGY_NAME = "ontology1"
-        io_manager.set_repository_location(REPOSITORY_LOCATION)
-        io_manager.set_context_member_name(IOContextMember.ONTOLOGY, ONTOLOGY_NAME)
-
-        error_msg = "Can not access variable data"
-
-        return io_manager, error_msg
-
-    # Indices, Variables and Equations are in the same file now, Indices
-    # are read first and is the one failing.
-
-    # @parametrize(folder_id=("2", "3"), ids=("invalid json", "invalid json schema"))
-    # def invalid_json_problem(
-    #     self,
-    #     io_manager: IOManager,
-    #     base_path: Path,
-    #     folder_id: str,
-    # ) -> tuple[IOManager, str]:
-    #     REPOSITORY_LOCATION = str(base_path / "repositoryOK")
-    #     ONTOLOGY_NAME = f"ontology{folder_id}"
-    #     io_manager.set_repository_location(REPOSITORY_LOCATION)
-    #     io_manager.set_context_member_name(IOContextMember.ONTOLOGY, ONTOLOGY_NAME)
-
-    #     error_msg = "Corrupted variable data"
-
-    #     return io_manager, error_msg
+    return ok_context, error_msg
 
 
-class TestIndexReading:
-    @parametrize_with_cases(
-        "manager, expected_variable_map", cases=CasesVariableReading, prefix="valid"
-    )
-    def test_index_reading_ok(
-        self, manager: IOManager, expected_variable_map: VariableMap
+class TestVariableReading:
+    def test_variable_reading_ok(
+        self, test_storage: storage.GenericStorage, ok_context: io.IOContext
     ) -> None:
-        output_variable_map = manager.get_current_variable_map()
+        expected_data = VARIABLE_DATA
 
-        assert expected_variable_map == output_variable_map
+        output_data = test_storage.get_variable_data(ok_context)
 
-    # @parametrize_with_cases(
-    #     "manager, error_msg", cases=CasesVariableReading, prefix="invalid"
-    # )
-    # def test_exception_on_missing_index_file(
-    #     self, manager: IOManager, error_msg: str
-    # ) -> None:
-    #     with pytest.raises(DataIOError, match=error_msg):
-    #         manager.get_current_variable_map()
+        assert expected_data == output_data
+
+    @pytest_cases.parametrize_with_cases("test_context, error_msg", cases=".")
+    def test_exception_on_missing_variable_file(
+        self,
+        test_storage: storage.GenericStorage,
+        test_context: io.IOContext,
+        error_msg: str,
+    ) -> None:
+        with pytest.raises(io.DataIOError, match=error_msg):
+            test_storage.get_variable_data(test_context)
