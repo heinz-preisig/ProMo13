c7bc8374ec1599dc85adaa83b6fb92fe7481b39c
Alberto Rodriguez Fernandez <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-27 13:46:32 +0100

    test(storage): rework repository_validation tests
    - Switch from testing the storage through the io_manager to test it directly.

--- tests/developer/common/io/storage/test_repository_validation.py
+++ tests/developer/common/io/storage/test_repository_validation.py
@@ -4,37 +4,35 @@ from collections.abc import Generator
 import pytest
 import pytest_cases
 
-from src.common import io
+from src.common.io import storage
+
+WRITE_ONLY_MODE = 0o222
+READ_ONLY_MODE = 0o444
 
 
-class CasesRepositoryValidation:
-    WRITE_ONLY_MODE = 0o222
-    READ_ONLY_MODE = 0o444
+def case_missing_directory(tmp_path: pathlib.Path) -> str:
+    return str(tmp_path / "MissingDirectory")
 
-    def case_missing_directory(self, tmp_path: pathlib.Path) -> str:
-        return str(tmp_path / "MissingDirectory")
 
-    @pytest_cases.parametrize(
-        mode=(WRITE_ONLY_MODE, READ_ONLY_MODE), ids=("write_only", "read_only")
-    )
-    def case_directory_permissions(
-        self, tmp_path: pathlib.Path, mode: int
-    ) -> Generator[str]:
-        dir_path = tmp_path / "Directory"
-        dir_path.mkdir(mode=mode)
+@pytest_cases.parametrize(
+    mode=(WRITE_ONLY_MODE, READ_ONLY_MODE), ids=("write_only", "read_only")
+)
+def case_directory_permissions(tmp_path: pathlib.Path, mode: int) -> Generator[str]:
+    dir_path = tmp_path / "Directory"
+    dir_path.mkdir(mode=mode)
 
-        yield str(dir_path)
+    yield str(dir_path)
 
-        FULL_PERMISSION_MODE = 0o777
-        dir_path.chmod(mode=FULL_PERMISSION_MODE)
+    FULL_PERMISSION_MODE = 0o777
+    dir_path.chmod(mode=FULL_PERMISSION_MODE)
 
 
 class TestRepositoryValidation:
-    @pytest_cases.parametrize_with_cases("location", cases=CasesRepositoryValidation)
+    @pytest_cases.parametrize_with_cases("location", cases=".")
     def test_exception_on_invalid_location(
-        self, io_manager: io.IOManager, location: str
+        self, test_storage: storage.GenericStorage, location: str
     ) -> None:
         msg = f"Invalid repository location: {location}"
 
-        with pytest.raises(io.DataIOError, match=msg):
-            io_manager.set_repository_location(location)
+        with pytest.raises(storage.DataIOError, match=msg):
+            test_storage.validate_repository_location(location)
