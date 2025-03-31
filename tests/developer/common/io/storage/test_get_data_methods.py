9c93f8c8e789ceaa4fc58a62be9a08b2b5bcd663
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-03-31 15:03:50 +0200

    test(storage): merge get data tests using parametrize

--- tests/developer/common/io/storage/test_get_data_methods.py
+++ tests/developer/common/io/storage/test_get_data_methods.py
@@ -1,10 +1,21 @@
-import pathlib
+import string
+import typing
 
 import pytest
 import pytest_cases
 
 from src.common import io
 from src.common.io import storage
+
+ACCESS_ERROR = string.Template("Can not access $data_name data")
+CORRUPTED_ERROR = string.Template("Corrupted $data_name data")
+
+INDEX_DATA = [
+    {"identifier": "I_1", "iri": "iri_I1", "label": "label_I1"},
+    {"identifier": "I_2", "iri": "iri_I2", "label": "label_I2"},
+    {"identifier": "I_3", "iri": "iri_I3", "label": "label_I3"},
+]
+
 
 VARIABLE_DATA = [
     {
@@ -94,41 +105,65 @@ VARIABLE_DATA = [
 ]
 
 
-ACCESS_ERROR = "Can not access variable data"
-CORRUPTED_ERROR = "Corrupted variable data"
+EQUATION_DATA = [
+    {"identifier": "E_1", "variables": ["V_1", "V_2", "V_3"]},
+    {"identifier": "E_6", "variables": ["V_1", "V_2", "V_5", "V_6"]},
+    {"identifier": "E_2", "variables": ["V_4", "V_5", "V_6"]},
+    {"identifier": "E_3", "variables": ["V_5", "V_1", "V_7"]},
+    {"identifier": "E_4", "variables": ["V_8", "V_9", "V_10"]},
+    {"identifier": "E_5", "variables": ["V_9", "V_4", "V_11", "V_12"]},
+]
+
+
+@pytest.fixture
+def expected_data_dict() -> dict[str, typing.Any]:
+    return {"index": INDEX_DATA, "variable": VARIABLE_DATA, "equation": EQUATION_DATA}
 
 
 @pytest_cases.case(id="")
 @pytest_cases.parametrize(
-    "name_id, error_msg",
+    "name_id, error_msg_template",
     [("1", ACCESS_ERROR), ("2", CORRUPTED_ERROR), ("3", CORRUPTED_ERROR)],
     ids=("missing file", "invalid json", "invalid json schema"),
 )
-def case_variable_file_problem(
-    ok_context: io.IOContext, name_id: str, error_msg: str
-) -> tuple[io.IOContext, str]:
+def case_index_file_problem(
+    ok_context: io.IOContext, name_id: str, error_msg_template: string.Template
+) -> tuple[io.IOContext, string.Template]:
     ONTOLOGY_NAME = f"ontology{name_id}"
     ok_context.ontology_name = ONTOLOGY_NAME
 
-    return ok_context, error_msg
+    return ok_context, error_msg_template
 
 
-class TestVariableReading:
-    def test_variable_reading_ok(
-        self, test_storage: storage.GenericStorage, ok_context: io.IOContext
+class TestGetDataMethods:
+    @pytest_cases.parametrize(data_name=("index", "variable", "equation"))
+    def test_get_data_ok(
+        self,
+        test_storage: storage.GenericStorage,
+        ok_context: io.IOContext,
+        expected_data_dict: dict[str, typing.Any],
+        data_name: str,
     ) -> None:
-        expected_data = VARIABLE_DATA
+        expected_data = expected_data_dict[data_name]
+        tested_method_name = f"get_{data_name}_data"
+        tested_method = getattr(test_storage, tested_method_name)
 
-        output_data = test_storage.get_variable_data(ok_context)
+        output_data = tested_method(ok_context)
 
         assert expected_data == output_data
 
-    @pytest_cases.parametrize_with_cases("test_context, error_msg", cases=".")
-    def test_exception_on_missing_variable_file(
+    @pytest_cases.parametrize(data_name=("index", "variable", "equation"))
+    @pytest_cases.parametrize_with_cases("test_context, error_msg_template", cases=".")
+    def test_exception_on_problem_with_data(
         self,
         test_storage: storage.GenericStorage,
         test_context: io.IOContext,
-        error_msg: str,
+        error_msg_template: string.Template,
+        data_name: str,
     ) -> None:
+        error_msg = error_msg_template.substitute(data_name=data_name)
+        tested_method_name = f"get_{data_name}_data"
+        tested_method = getattr(test_storage, tested_method_name)
+
         with pytest.raises(io.DataIOError, match=error_msg):
-            test_storage.get_variable_data(test_context)
+            tested_method(test_context)
