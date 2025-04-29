de3891f15eab7648267445c9d361d16a92625379
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-04-29 14:36:36 +0200

    feat(corelib): add new types to data_types
    - add CoreMap to help with type hinting where one of the maps is expected
    - add CoreMapVariant to help identify the type of CoreMap expected

--- src/common/corelib/__init__.py
+++ src/common/corelib/__init__.py
@@ -6,9 +6,18 @@ ProMo. It includes essential building blocks that define the core data
 structures and interfaces used throughout the application.
 """
 
-from .data_types import EquationMap, IndexMap, VariableMap
+from .data_types import CoreMap, CoreMapVariant, EquationMap, IndexMap, VariableMap
 from .equation import Equation
 from .index import Index
 from .variable import Variable
 
-__all__ = ["Index", "IndexMap", "Variable", "VariableMap", "Equation", "EquationMap"]
+__all__ = [
+    "CoreMap",
+    "CoreMapVariant",
+    "Index",
+    "IndexMap",
+    "Variable",
+    "VariableMap",
+    "Equation",
+    "EquationMap",
+]
--- src/common/corelib/data_types.py
+++ src/common/corelib/data_types.py
@@ -1,5 +1,14 @@
+import enum
+
 from src.common.corelib import equation, index, variable
 
 type IndexMap = dict[str, index.Index]
 type VariableMap = dict[str, variable.Variable]
 type EquationMap = dict[str, equation.Equation]
+type CoreMap = IndexMap | VariableMap | EquationMap
+
+
+class CoreMapVariant(enum.StrEnum):
+    INDEX = "index"
+    VARIABLE = "variable"
+    EQUATION = "equation"
