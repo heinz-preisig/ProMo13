7fa04db1e601c31ff2b815ae1fce622f7a500cd2
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-05-23 11:48:45 +0200

    feat(components): add a model to store the available ontologies

--- src/common/components/starting_dialog/model.py
+++ src/common/components/starting_dialog/model.py
@@ -1,6 +1,16 @@
-from src.common.io import IOManager
+from PySide6 import QtCore
+
+from src.common import io
 
 
 class StartingDialogModel:
-    def __init__(self, manager: IOManager) -> None:
+    def __init__(self, manager: io.IOManager) -> None:
         self._manager = manager
+        self._ontologies_model = QtCore.QStringListModel()
+
+    def load_available_ontologies(self) -> None:
+        print("here")
+        available_ontologies = self._manager.get_context_member_valid_options(
+            io.IOContextMember.ONTOLOGY
+        )
+        self._ontologies_model.setStringList(available_ontologies)
