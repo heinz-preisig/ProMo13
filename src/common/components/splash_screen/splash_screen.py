233a797e59b544a63b0b33b3ceacb0bfe4002f2c
AlbertoRFer <17274024+AlbertoRFer@users.noreply.github.com>
2025-05-28 19:36:20 +0200

    feat(bl): add splash screen

---
+++ src/common/components/splash_screen/splash_screen.py
@@ -0,0 +1,26 @@
+import enum
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
+
+class PromoModule(enum.StrEnum):
+    LINKER = "../packages/Common/icons/task_entity_generation.svg"
+
+
+SPLASH_SCREEN_SIZE = QtCore.QSize(300, 300)
+
+
+class ClickableSplashScreen(QtWidgets.QSplashScreen):
+    clicked = QtCore.pyqtSignal()
+
+    def __init__(self, module: PromoModule) -> None:
+        pixmap = QtGui.QPixmap(module).scaled(
+            SPLASH_SCREEN_SIZE,
+            Qt.AspectRatioMode.KeepAspectRatio,
+            Qt.TransformationMode.SmoothTransformation,
+        )
+        super().__init__(pixmap)
+
+    def mousePressEvent(self, a0: QtGui.QMouseEvent | None) -> None:
+        self.clicked.emit()
