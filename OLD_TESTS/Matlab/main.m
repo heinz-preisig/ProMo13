c65737d129fc35f085a00042f9f36a5d27f61b32
Heinz A Preisig <heinz.preisig@chemeng.ntnu.no>
2025-08-08 13:10:57 +0200

    [ProMo] moved OLDTESTS to attic_ProMo outside of the project

--- OLD_TESTS/Matlab/main.m
+++
@@ -1,8 +0,0 @@
-function main
-  indexOrder = {"A", "B", "C", "D"};
-  V = MultiDimVar({"A"}, [5], indexOrder, [1; 2; 3; 4; 5]);
-  V(2) = 348
-
-
-
-
--- OLD_TESTS/unit/test_utils.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-def get_test_files_path() -> Path:
-  """Return the test_files directory."""
-  return Path(__file__).resolve().parent / 'test_files'
